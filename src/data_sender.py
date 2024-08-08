8bc58c5867429efcdc8b1369dda68857aa26a87b
idokoren0 <145870791+idokoren0@users.noreply.github.com>
2024-08-08 19:44:07 +0300

    added encryption

--- src/data_sender.py
+++ src/data_sender.py
@@ -1,6 +1,7 @@
 import socket
-import requests
+import ssl
 import logging
+import requests
 
 class DataSender:
     def __init__(self, config):
@@ -15,19 +16,27 @@ class DataSender:
 
     def send_data_http(self, enriched_data):
         try:
-            response = requests.post(f"http://{self.config.target_server}:{self.config.target_port}", json=enriched_data)
+            response = requests.post(
+                f"https://{self.config.target_server}:{self.config.target_port}",
+                json=enriched_data,
+                verify='/app/server.crt'  # Make sure to use the correct path to your server certificate
+            )
             response.raise_for_status()
-            self.logger.info("Data sent successfully via HTTP")
+            self.logger.info("Data sent successfully via HTTPS")
         except requests.exceptions.RequestException as e:
-            self.logger.error(f"Error sending data via HTTP: {e}")
+            self.logger.error(f"Error sending data via HTTPS: {e}")
 
     def send_data_tcp(self, enriched_data):
+        context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        context.load_verify_locations('/app/server.crt')
+
         target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            target_socket.connect((self.config.target_server, self.config.target_port))
-            target_socket.sendall(enriched_data.encode('utf-8'))
-            self.logger.info("Data sent successfully via TCP")
+            wrapped_socket = context.wrap_socket(target_socket, server_hostname=self.config.target_server)
+            wrapped_socket.connect((self.config.target_server, self.config.target_port))
+            wrapped_socket.sendall(enriched_data.encode('utf-8'))
+            self.logger.info("Data sent successfully via TCP with TLS")
         except Exception as e:
-            self.logger.error(f"Error sending data via TCP: {e}")
+            self.logger.error(f"Error sending data via TCP with TLS: {e}")
         finally:
-            target_socket.close()
+            wrapped_socket.close()
